6998f8d2d190d7278fb97a209f23744a452ef90f
jai3738 <jai.singh@techcurl.com>
2020-04-15 13:28:15 +0530

    delete unusable files

--- angular-7-crud/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,  
-  baseUrl: 'http://jaiksingh.com/angular7crud/'
-};
--- angular-7-crud/environments/environment.stage.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,  
-  baseUrl: 'http://jaiksingh.com/angular7crud/'
-};
