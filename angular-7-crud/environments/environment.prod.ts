fbc6bd41c5a07888bf79743af62fc4783ecfd05d
jai3738 <jai.singh@techcurl.com>
2020-04-15 13:04:41 +0530

    add commands sheet and add environment.ts

---
+++ angular-7-crud/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,  
+  baseUrl: 'http://jaiksingh.com/angular7crud/'
+};
---
+++ angular-7-crud/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,  
+  baseUrl: 'http://jaiksingh.com/angular7crud/'
+};
